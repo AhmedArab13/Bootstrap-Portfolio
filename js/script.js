ae730fa4bef4a5cfa41a0e751a55937c0de6b716
AhmedArab13 <112523754+AhmedArab13@users.noreply.github.com>
2024-03-19 02:46:19 +0200

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,7 @@
+var typed = new Typed(".auto-type" , {
+    strings : [ "Programmer" , "Backend developer " , "Cyber security engineer" , "Penetration tester" ] ,
+    typeSpeed : 100 ,
+    backSpeed : 100 ,
+    looped : true ,
+    
+  }) ;
